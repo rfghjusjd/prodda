79388cbb93553332351c74c7b98d7f6b5f254438
rfghjusjd <47795384+rfghjusjd@users.noreply.github.com>
2019-02-19 21:16:43 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("547403602575097875")
+setInterval(function() {
+channel.send(` gg gg  gggg   ggggggggggggggg gggggggggggg ggggggggggاحبكم احبكم 1الف :heart: `);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
