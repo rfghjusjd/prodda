e43beaa3f43af8eca22271b5b308fe91e668911b
rfghjusjd <47795384+rfghjusjd@users.noreply.github.com>
2019-02-19 21:37:48 +0100

    Add files via upload

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ console.log("Scrpit By Dream");
 client.on("ready", () => {
 let channel =     client.channels.get("546476299137318917")
 setInterval(function() {
-channel.send(` gg gg  gggg   ggggggggggggggg gggggggggggg ggggggggggاحبكم احبكم 1الف :heart: `);
+channel.send(`by [مجهول/nasto] GG`);
 }, 30)
 })
 
